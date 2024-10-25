3e9f60163b7f18fcf3a481e3b75defcdf19f206a
florianbuetow <2320560+florianbuetow@users.noreply.github.com>
2024-10-25 03:42:54 +0200

    Adding comments

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Builds the image (if it doesnt exist) and starts the containers
 docker compose up --build
--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Stops the containers and removes the image that was built
 docker compose down --rmi all
