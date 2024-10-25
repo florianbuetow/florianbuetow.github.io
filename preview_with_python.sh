de1403f946242cb32498949d2ab246e762cf955f
florianbuetow <2320560+florianbuetow@users.noreply.github.com>
2024-10-25 13:18:59 +0200

    Adding preview script not based on Docker

--- preview_with_python.sh
+++ preview_with_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Builds the image (if it doesnt exist) and starts the container
+# Starts a python simple http server to serve the files in the docs folder
 PORT=8000 # If you change the port in the docker-compose.yml file, you need to change it here as well.
 (sleep 1 ; open http://localhost:$PORT) &
-docker compose up preview
+python3 -m http.server $PORT -d ./docs/
