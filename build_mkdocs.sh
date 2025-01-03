dc5d4f90bb61bcab503ee3f2a471016eea7d62a3
florianbuetow <2320560+florianbuetow@users.noreply.github.com>
2025-01-03 01:31:37 +0100

    Deployed f2f6af3 with MkDocs version: 1.6.1

--- build_mkdocs.sh
+++
@@ -1,3 +0,0 @@
-pip install -r requirements-doc.txt
-./build_notebooks.sh
-mkdocs build
--- build_notebooks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Convert all notebooks to Markdown
-for notebook in docs/examples/*.ipynb; do
-    jupyter nbconvert --to markdown "$notebook"
-done
